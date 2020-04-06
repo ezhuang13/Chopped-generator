592ea1f1bfd83f34f70c98536d0c58b2569aa83b
Eric Zhuang <erzhuang@microsoft.com>
2020-04-05 17:30:27 -0700

    adding linter, reformatting to typescript and functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,73 @@
 import * as React from 'react';
 import './App.css';
+import { meats, vegetables, sides } from './foods';
 
-const meats = ["Tuna", "Salmon", "Octopus", "Scallop", "Squid", "Cod", "Clam",
-              "Chicken Thigh", "Chicken Breast", "White Fish",
-              "Turkey Breast", "Turkey Drumstick",
-              "Pork Ribs", "Pork Shoulder", "Pork Belly", "Pork Chop",
-              "Duck Breast", 
-              "Chuck Steak", "Beef Brisket",
-              "Lamb Shanks", "Shrimp","Prawns",
-              "Rabbit", "Pigeon", "Quail","Goose","Venison"];
+const App = () => {
+    const [basket, setBasket] = React.useState({
+        meat: '',
+        vegetable: '',
+        side1: '',
+        side2: '',
+    });
+    const [saved, setSaved] = React.useState({
+        meat: '',
+        vegetable: '',
+        side1: '',
+        side2: '',
+    });
 
-const vegetables = ["Cauliflower", "Egg Plant", "Leeks", "Sweet Potato",
-                    "Butternut Squash", "Shallots","Bok Choy", "Cherry Tomato", "Cactus", 
-                    "Crimini Mushroom", "Portobello Mushroom", "Okra", "Kale"];
-
-const sides = ["Red Wine", "Avocado",
-              "Corn tortillas", "Mozarella", "Pickles","Wasabi","Gochujang", "Trail Mix",
-              "Basil", "Cashews", "Dried cherries", "Chocolate covered Espresso beans",
-              "Applesauce","Chips","Orange","Peaches","Old Bay Seasoning","Prosciutto"];
-
-class App extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = { 
-      basket: { meat: '', vegetable: '', side1: '', side2: ''  },
-      saved: { meat: '', vegetable: '', side1: '', side2: '' }
+    const getRandomIngredient = (foodArray: string[]) => {
+        return foodArray[Math.floor(Math.random() * foodArray.length)];
     };
-  }
 
-  getRandomIngredient = (array: string[]) => {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-  
-  generateBasket = () => {
-    const newSide1 = this.getRandomIngredient(sides);
-    let newSide2 = this.getRandomIngredient(sides);
-    while (newSide2 === newSide1) {
-      newSide2 = this.getRandomIngredient(sides);
-    }
-    this.setState({ basket: {
-      meat: this.getRandomIngredient(meats),
-      vegetable: this.getRandomIngredient(vegetables),
-      side1: newSide1,
-      side2: newSide2,
-    } });
-  }
+    const generateBasket = () => {
+        const newSide1 = getRandomIngredient(sides);
+        let newSide2 = getRandomIngredient(sides);
+        while (newSide2 === newSide1) {
+            newSide2 = getRandomIngredient(sides);
+        }
+        setBasket({
+            meat: getRandomIngredient(meats),
+            vegetable: getRandomIngredient(vegetables),
+            side1: newSide1,
+            side2: newSide2,
+        });
+    };
 
-  saveIngredient = (category: string) => {
-    let saved = { ...this.state.saved };
-    saved[category] = this.state.basket[category];
-    this.setState({ saved: saved });
-  }
-  render () {
-  return (
-      <div className="App">
-        <button onClick={this.generateBasket}>Generator chopped basket</button>
-        <div>Random basket:</div>
-        <div className={"btn-group"}>
-          {
-            Object.keys(this.state.basket).map((category) => {
-              if (this.state.basket[category] === '') return null;
-              let selected = this.state.basket[category] === this.state.saved[category];
-              return <button className={"btn"} disabled={selected} onClick={() => this.saveIngredient(category)}>
-                      { this.state.basket[category] }
-                    </button>
-            })
-          }
+    const saveIngredient = (category: string) => {
+        setSaved({ ...saved, [category]: basket[category] });
+    };
+    return (
+        <div className="App">
+            <button onClick={generateBasket}>Generator chopped basket</button>
+            <div>Random basket:</div>
+            <div className={'btn-group'}>
+                {Object.keys(basket).map((category) => {
+                    if (basket[category] === '') return null;
+                    let selected = basket[category] === saved[category];
+                    return (
+                        <button
+                            className={'btn'}
+                            disabled={selected}
+                            onClick={() => saveIngredient(category)}>
+                            {basket[category]}
+                        </button>
+                    );
+                })}
+            </div>
+            <div className={'saved'}>
+                <div>Saved ingredients:</div>
+                {Object.keys(basket).map((category) => (
+                    <div>
+                        <span style={{ textTransform: 'capitalize' }}>
+                            {category}:{' '}
+                        </span>
+                        {saved[category]}
+                    </div>
+                ))}
+            </div>
         </div>
-        <div className={"saved"}>
-          <div>Saved ingredients:</div>
-          {
-            Object.keys(this.state.basket).map((category) => 
-              <div>
-                <span style={{textTransform: 'capitalize'}}>{category}: </span>
-                { this.state.saved[category] }
-              </div>)
-          }
-        </div>
-      </div>
     );
-  }
-}
+};
 
 export default App;
